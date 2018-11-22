756b1d12ca364865aeb36abd83f41583239dcaab
Unknown <simon.bonnaud@gmail.com>
2018-11-22 08:38:10 +0100

    Chargement de la page web fonctionnel

--- app/src/main/java/com/example/bonnaudgastard/projet_bonnaud_gastard/MainActivity.java
+++ app/src/main/java/com/example/bonnaudgastard/projet_bonnaud_gastard/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.bonnaudgastard.projet_bonnaud_gastard;
 
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Observer {
+    ChapterData data = new ChapterData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data.observe(this, this);
+    }
+
+    @Override
+    public void onChanged(@Nullable Object o) {
+
     }
 }
