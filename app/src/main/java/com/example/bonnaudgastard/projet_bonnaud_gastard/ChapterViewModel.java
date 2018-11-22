07ecc41badad4523b3d6dd17545ebaac954bfdc7
Sidonie <sgastard@enssat.fr>
2018-11-22 09:25:21 +0100

    Ajout de l'objet chapter et début du VM (non fonctionnel) SUITE

--- app/src/main/java/com/example/bonnaudgastard/projet_bonnaud_gastard/ChapterViewModel.java
+++ app/src/main/java/com/example/bonnaudgastard/projet_bonnaud_gastard/ChapterViewModel.java
@@ -2,5 +2,8 @@ package com.example.bonnaudgastard.projet_bonnaud_gastard;
 
 import android.arch.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class ChapterViewModel extends ViewModel {
+    private List<Chapter> chapterList;
 }
