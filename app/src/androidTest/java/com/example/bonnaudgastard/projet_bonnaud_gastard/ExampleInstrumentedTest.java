82e0a5fd5f9ca1bf2c3f79e499ab5e61f259444e
Unknown <simon.bonnaud@gmail.com>
2019-01-17 10:33:12 +0100

    Ajout des dépendances + début des tests

--- app/src/androidTest/java/com/example/bonnaudgastard/projet_bonnaud_gastard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bonnaudgastard/projet_bonnaud_gastard/ExampleInstrumentedTest.java
@@ -4,8 +4,14 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.test.espresso.web.assertion.WebViewAssertions;
+import androidx.test.espresso.web.sugar.Web;
+import androidx.test.espresso.web.webdriver.DriverAtoms;
+import androidx.test.espresso.web.webdriver.Locator;
 
 import static org.junit.Assert.*;
 
@@ -22,5 +28,26 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.bonnaudgastard.projet_bonnaud_gastard", appContext.getPackageName());
+    }
+
+    @Test
+    public void URLisCorrect(){
+        Web.onWebView().withElement(DriverAtoms.findElement(Locator.ID, "webview"))
+            .check(WebViewAssertions.webMatches(DriverAtoms.getText(), CoreMatchers.containsString("Big Buck Bunny")));
+    }
+
+    @Test
+    public void buttonsCreated(){
+
+    }
+
+    @Test
+    public void videoCreated(){
+
+    }
+
+    @Test
+    public void buttonsWorking(){
+
     }
 }
