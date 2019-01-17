91840e8e5dfb7aac3f853c9c6d9b99255ea83a3e
Unknown <simon.bonnaud@gmail.com>
2019-01-17 11:05:51 +0100

    Branch

--- app/src/androidTest/java/com/example/bonnaudgastard/projet_bonnaud_gastard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bonnaudgastard/projet_bonnaud_gastard/ExampleInstrumentedTest.java
@@ -1,12 +1,20 @@
 package com.example.bonnaudgastard.projet_bonnaud_gastard;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.web.webdriver.Locator;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
+import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +24,38 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Before
+    public void launchActivity() {
+        ActivityScenario.launch(MainActivity.class);
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.bonnaudgastard.projet_bonnaud_gastard", appContext.getPackageName());
+    }
+
+    @Test
+    public void URLisCorrect(){
+        onWebView().withElement(findElement(Locator.ID, "webview"))
+            .check(webMatches(getText(), containsString("Big Buck Bunny")));
+    }
+
+    @Test
+    public void buttonsCreated(){
+
+    }
+
+    @Test
+    public void videoCreated(){
+
+    }
+
+    @Test
+    public void buttonsWorking(){
+
     }
 }
